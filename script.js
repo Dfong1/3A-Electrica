c60618c0baa914ab82496502456ac9486e174e2e
TecnoDev1 <tecnodev12@gmail.com>
2024-05-28 01:25:10 -0600

    Diseño principal

---
+++ script.js
@@ -0,0 +1,9 @@
+const instagram = document.getElementById('instagram');
+const facebook = document.getElementById('facebook');
+
+facebook.addEventListener('click', function(e) {
+    window.open("https://www.facebook.com/3aelectrica", "_blank")
+})
+
+
+
