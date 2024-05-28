1cf5f4f7d6d82d644b49dad6a3f61a9fc6d2cec6
TecnoDev1 <tecnodev12@gmail.com>
2024-05-28 01:27:35 -0600

    PENDIENTES

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ facebook.addEventListener('click', function(e) {
     window.open("https://www.facebook.com/3aelectrica", "_blank")
 })
 
-
+// TODO: SI ES POSIBLE AÑADIR PARA QUE MANDE CORREO Y PARA QUE MANDE AL TELEFONO
 
